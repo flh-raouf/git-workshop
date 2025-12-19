30462f8813694b91bbbd0d8a466564a248bf3ae6
Abderraouf FELLAHI <ma_fellahi@esi.dz>
2025-12-19 20:31:01 +0100

    add a+b

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
 console.log('hello world')
+
+let a = 5
+let b = 10
+console.log(a + b)
+
+// git add .
+// git commit -m 'add addition fonction'
+// git push
+
+// git remote add origin
+// git branch
+// git init
